c288e621071008c2353b07b51cd0ac87066d7f2a
OlgaTyupina <57899824+OlgaTyupina@users.noreply.github.com>
2019-12-17 16:13:27 +0300

    Update CardDeliveryTest.java

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,10 +1,6 @@
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
-import javax.xml.crypto.Data;
-import java.util.Calendar;
-import java.util.Date;
-
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
@@ -14,7 +10,6 @@ class CardDeliveryTest {
     void shouldFormSubmission() {
         open("http://localhost:9999");
         SelenideElement form = $ ("form.form");
-        //$$(".tab-item").find(exactText("По номеру счёта")).click();
         form.$("[data-test-id=city] input").setValue("Москва");
         form.$("[data-test-id=date] input").setValue("20.12.2019");
         form.$("[data-test-id=name] input").setValue("Иван Иванов");
@@ -22,8 +17,5 @@ class CardDeliveryTest {
         form.$("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
         $("[data-test-id=notification]").waitUntil(visible, 15000);
-
     }
-
-
 }
