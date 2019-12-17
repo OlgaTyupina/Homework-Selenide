fd6a3eec17d8e7816215684c987c55f2c1b94b62
Olga Tyupina <olga.tyupina@gmail.com>
2019-12-17 16:23:46 +0300

    Changing permission of gradlew

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,9 +1,6 @@
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
-import javax.xml.crypto.Data;
-import java.util.Calendar;
-import java.util.Date;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
@@ -14,7 +11,6 @@ class CardDeliveryTest {
     void shouldFormSubmission() {
         open("http://localhost:9999");
         SelenideElement form = $ ("form.form");
-        //$$(".tab-item").find(exactText("По номеру счёта")).click();
         form.$("[data-test-id=city] input").setValue("Москва");
         form.$("[data-test-id=date] input").setValue("20.12.2019");
         form.$("[data-test-id=name] input").setValue("Иван Иванов");
